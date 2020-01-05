3b827f594152d1e6381944c2784122cc03141dd0
jxtxzzw <jxtxzzw@126.com>
2020-01-05 20:24:28 +0800

    force start 0-9
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- byteps/common/scheduled_queue.h
+++ byteps/common/scheduled_queue.h
@@ -47,7 +47,7 @@ namespace byteps {
 
             uint32_t pendingSize();
 
-            void reportFinish(int size);
+            void reportFinish(std::shared_ptr <TensorTableEntry> task);
 
         private:
             struct comparator {
