e85929c47885345e1d8420cb8b461c9dcd813440
jxtxzzw <jxtxzzw@126.com>
2020-01-09 19:44:06 +0800

    use door
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- byteps/common/scheduled_queue.cc
+++ byteps/common/scheduled_queue.cc
@@ -19,185 +19,277 @@
 #include "logging.h"
 
 namespace byteps {
-namespace common {
+    namespace common {
 
-BytePSScheduledQueue::BytePSScheduledQueue(QueueType type) {
-//  if (type == REDUCE && BytePSGlobal::GetNccl()->IsSignalRoot()) {
-//    _is_scheduled = true;
-//  } else {
-//    _is_scheduled = false;
-//  }
-_is_scheduled = true;
-  size_t credit_in_partition = BytePSGlobal::GetNccl()->GetGroupSize() + 1;
+        BytePSScheduledQueue::BytePSScheduledQueue(QueueType type) {
 
-  auto byteps_scheduling_credit = getenv("BYTEPS_SCHEDULING_CREDIT");
-  credit_in_partition = byteps_scheduling_credit ? atoi(byteps_scheduling_credit) : 0;
-//  if (!credit_in_partition) { // disable scheduling by default
-//    _is_scheduled = false;
-//  }
+            if (type == REDUCE && BytePSGlobal::GetNccl()->IsSignalRoot()) {
+                _is_scheduled = true;
+            } else {
+                _is_scheduled = false;
+            }
 
-  _qt = type;
-  _credits = _is_scheduled
-              ? BytePSGlobal::GetPartitionBound() * credit_in_partition
-              : 34359738368;  // 32GB, basically disabling credit control
-  _rt = nullptr;
+            size_t credit_in_partition = BytePSGlobal::GetNccl()->GetGroupSize() + 1;
+            if (getenv("BYTEPS_SCHEDULING_CREDIT")) {
+                credit_in_partition = atoi(getenv("BYTEPS_SCHEDULING_CREDIT"));
+            }
+            if (!credit_in_partition) {
+                _is_scheduled = false;
+            }
 
-  switch (_qt) {
-    case REDUCE:
-      if (BytePSGlobal::GetNccl()->IsSignalRoot()) {
-        _rt = BytePSGlobal::GetReduceTable();
-      }
-      break;
-    case PCIE_REDUCE:
-      if (BytePSGlobal::IsCrossPcieSwitch()) {
-        if (BytePSGlobal::GetCpuReducer()->isRoot()) {
-          _rt = BytePSGlobal::GetPcieReduceTable();
+            _qt = type;
+            _credits = _is_scheduled
+                       ? BytePSGlobal::GetPartitionBound() * credit_in_partition
+                       : 34359738368;  // 32GB, basically disabling credit control
+            _rt = nullptr;
+
+            switch (_qt) {
+                case REDUCE:
+                    if (BytePSGlobal::GetNccl()->IsSignalRoot()) {
+                        _rt = BytePSGlobal::GetReduceTable();
+                    }
+                    break;
+                case PCIE_REDUCE:
+                    if (BytePSGlobal::IsCrossPcieSwitch()) {
+                        if (BytePSGlobal::GetCpuReducer()->isRoot()) {
+                            _rt = BytePSGlobal::GetPcieReduceTable();
+                        }
+                    }
+                    break;
+                case PUSH:
+                    if (BytePSGlobal::IsRootDevice()) {
+                        _rt = BytePSGlobal::GetPushTable();
+                    }
+                    if(getenv("Z_BATCH_SIZE"))
+                        batchsize = atoi(getenv("Z_BATCH_SIZE"));
+                    if(getenv("MODEL")) {
+                        if (!strcmp(getenv("MODEL"), "vgg19")) {
+                            int tmp1[13] = {-1, 1, 13, 27, 37, 0, 77, 90, 103, 117, 130, 143, 156};
+                            double tmp2[13] = {285.4, 196.2, 33.2, 0, 0, 53, 44, 64, 90, 74, 58, 15, 0}; // backward execution time
+                            _init_pointer = 4;
+                            for (int i = 0; i <= _init_pointer; i++) {
+                                _grad_checkpoint[i] = tmp1[i];
+                                _backward_exec[i] = tmp2[i];
+                            }
+                            B *= 4;
+                            begin_name = "DistributedGradientDescentOptimizer_Push_Pull/BytePSPushPull_gradients_vgg16_predictions_BiasAdd_grad_tuple_control_dependency_1_0";
+                        }
+                    }
+                    _pointer = _init_pointer;
+                    expected_priority = _grad_checkpoint[_pointer];
+                    for (int i = 0; i < 13; i++) {
+                        _backward_exec[i] *= (double)batchsize/64;
+                    }
+                    for (int i = 0; i < 13; i++) {
+                        _backward_exec[i] *= B;
+                    }
+                    break;
+                case COPYH2D:
+                    if (!BytePSGlobal::IsRootDevice()) {
+                        _rt = BytePSGlobal::GetCopyTable();
+                    }
+                    break;
+                case BROADCAST:
+                    if (BytePSGlobal::GetNccl()->IsSignalRoot()) {
+                        _rt = BytePSGlobal::GetBroadcastTable();
+                    }
+                    break;
+                case PULL:
+                    break;
+                default:
+                    break;
+            }
         }
-      }
-      break;
-    case PUSH:
-      if (BytePSGlobal::IsRootDevice()) {
-        _rt = BytePSGlobal::GetPushTable();
-      }
-      break;
-    case COPYH2D:
-      if (!BytePSGlobal::IsRootDevice()) {
-        _rt = BytePSGlobal::GetCopyTable();
-      }
-      break;
-    case BROADCAST:
-      if (BytePSGlobal::GetNccl()->IsSignalRoot()) {
-        _rt = BytePSGlobal::GetBroadcastTable();
-      }
-      break;
-    default:
-      break;
-  }
-}
 
-void BytePSScheduledQueue::addTask(std::shared_ptr<TensorTableEntry> entry) {
-  std::lock_guard<std::mutex> lock(_mutex);
-  _sq.push_back(entry);
-//  if (_is_scheduled) {
-    // TODO: below can be optimized to O(n) using insertion sort
-    std::sort(
-        _sq.begin(), _sq.end(),
-        [](std::shared_ptr<TensorTableEntry> a,
-           std::shared_ptr<TensorTableEntry> b) {
-          if (a->priority == b->priority) {
-            return (a->key < b->key);  // from the first partition to the last
-          }
-          return (a->priority > b->priority);  // from higher priority to lower
-        });
-//  }
-  BPS_CHECK(entry->tensor_name != "");
-  BPS_LOG(TRACE) << "Queue " << LogStrings[_qt]
-                 << " addTask: " << entry->tensor_name << " key: " << entry->key
-                 << " rank: " << BytePSGlobal::GetLocalRank();
-  return;
-}
+        void BytePSScheduledQueue::addTask(std::shared_ptr <TensorTableEntry> entry) {
+            std::lock_guard <std::mutex> lock(_mutex);
+            if (_qt == PUSH && (entry->tensor_name).find("gradient") != (entry->tensor_name).npos) {
+                _ms.insert(entry);
+                long long now = getSystemTime();
+//                BPS_LOG(INFO) << "PUSH gradient operation is ready: " << entry -> priority << " now:" << now << "  next_timer" << next_timer;
+                _tensor_part[entry->priority * -1] = entry->total_partnum;
+                if ((entry->tensor_name).find(begin_name) != (entry->tensor_name).npos) {
+                    timer = getSystemTime();
+                    duration_ptr = 0;
+                    _dooropen = 11;
+                    next_timer = timer + durations[duration_ptr];  // next_timer is the endline of this stage.
+                    dynamic_size =  durations[duration_ptr] * B;
+//                    BPS_LOG(INFO) << "start!!";
+                }
+            } else {
+                _sq.push_back(entry);
+            }
+            BPS_CHECK(entry->tensor_name != "");
+            BPS_LOG(DEBUG) << "Queue " << LogStrings[_qt]
+                           << " addTask: " << entry->tensor_name << " key: " << entry->key
+                           << " rank: " << BytePSGlobal::GetLocalRank();
+            return;
+        }
 
-// Record the start time of the sub-tasks for all QueueTypes of each partition.
-void BytePSScheduledQueue::recorderTs(std::shared_ptr<TensorTableEntry> task) {
-  auto context = task->context;
-  // add for profiling
-  if (context->profile_flag) {
-    auto now = std::chrono::system_clock::now();
-    auto duration = now.time_since_epoch();
-    auto us = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+        void BytePSScheduledQueue::recorderTs(std::shared_ptr <TensorTableEntry> task) {
+            auto context = task->context;
+            if (context->profile_flag) {
+                auto now = std::chrono::system_clock::now();
+                auto duration = now.time_since_epoch();
+                auto us = std::chrono::duration_cast<std::chrono::microseconds>(duration);
 
-    auto &queue_list = task->queue_list;
-    BPS_CHECK_GE(queue_list.size(), 1);
-    auto this_op = queue_list[0];
+                auto &queue_list = task->queue_list;
+                BPS_CHECK_GE(queue_list.size(), 1);
+                auto this_op = queue_list[0];
 
-    BPSCommTime *ret = new BPSCommTime;
-    ret->start_t = (long long)(us.count());
-    ret->key = task->key;
-    ret->type = this_op; 
-    context->part_comm_time[task->key][this_op].push(ret);
-  }
-}
+                BPSCommTime *ret = new BPSCommTime;
+                ret->start_t = (long long) (us.count());
+                ret->key = task->key;
+                ret->type = this_op;
+                context->part_comm_time[task->key][this_op].push(ret);
+            }
+        }
 
-std::shared_ptr<TensorTableEntry> BytePSScheduledQueue::getTask() {
-  std::lock_guard<std::mutex> lock(_mutex);
-  std::shared_ptr<TensorTableEntry> task;
-  // TODO: below can be optimized -- if we take task from the tail, erase() can
-  // be faster
-  for (auto it = _sq.begin(); it != _sq.end(); ++it) {
-    if ((*it)->ready_event) {
-      if (!(*it)->ready_event->Ready()) {
-        continue;
-      }
-    }
-//    if (_is_scheduled) {
-      if ((*it)->len > _credits) {
-        continue;
-      }
-//    }
-    if (_rt) {
-      if (!_rt->IsKeyReady((*it)->key)) {
-        continue;
-      }
-      _rt->ClearReadyCount((*it)->key);
-    }
-    task = *it;
-    _sq.erase(it);
-//    if (_is_scheduled) {
-      _credits -= task->len;
-//    }
+        struct isTargetPriority {
+            int Priority;
 
-    BPS_CHECK(task->tensor_name != "");
-    BPS_LOG(TRACE) << "Queue " << LogStrings[_qt]
-                   << " getTask: " << task->tensor_name << " key: " << task->key
-                   << " rank: " << BytePSGlobal::GetLocalRank();
-    task->ready_event = nullptr;
-    // Add for profiling communication traces
-    recorderTs(task);
-    return task;
-  }
-  return nullptr;
-}
+            isTargetPriority(int priority) : Priority(priority) {}
 
+            bool operator()(std::shared_ptr <TensorTableEntry> x) {
+                return x->priority == Priority;
+            }
+        };
 
-std::shared_ptr<TensorTableEntry> BytePSScheduledQueue::getTask(uint64_t key) {
-//  BPS_CHECK(!_is_scheduled);
-  std::lock_guard<std::mutex> lock(_mutex);
-  std::shared_ptr<TensorTableEntry> task;
-  for (auto it = _sq.begin(); it != _sq.end(); ++it) {
-    if ((*it)->ready_event) {
-      BPS_CHECK((*it)->ready_event->Ready());
-    }
-    if ((*it)->key != (uint64_t)key) {
-      continue;
-    }
-    task = *it;
-    _sq.erase(it);
+        std::multiset < std::shared_ptr < TensorTableEntry >> ::iterator BytePSScheduledQueue::findTask(int priority) {
+            std::shared_ptr<TensorTableEntry> e(new TensorTableEntry);
+            e->priority = priority;
+            std::multiset < std::shared_ptr < TensorTableEntry >> ::iterator
+            it = _ms.lower_bound(e);
+            if (it == _ms.end()) {
+                return it;
+            } else if ((*it)->priority != priority) {
+                return _ms.end();
+            } else {
+                BPS_CHECK_EQ((*it)->priority, priority);
+                return it;
+            }
+        }
 
-    BPS_CHECK(task->tensor_name != "");
-    BPS_LOG(TRACE) << "Queue " << LogStrings[_qt]
-                   << " getTask(key): " << task->tensor_name
-                   << " key: " << task->key
-                   << " rank: " << BytePSGlobal::GetLocalRank();
-    task->ready_event = nullptr;
-    // Add for profiling communication traces
-    recorderTs(task);
-    return task;
-  }
-  return nullptr;
-}
+        std::shared_ptr <TensorTableEntry> BytePSScheduledQueue::getTask() {
+            std::lock_guard <std::mutex> lock(_mutex);
+            std::shared_ptr <TensorTableEntry> task;
+            std::multiset < std::shared_ptr < TensorTableEntry >> ::iterator msit;
+            if (_qt == PUSH && _ms.size() > 0) {
+//                long long now = getSystemTime();
+////                BPS_LOG(INFO) << "now:" << now << " ,next_timer" << next_timer;
+//                if (now <= next_timer || duration_ptr == duration_ptr_len) {
+//                    msit = _ms.begin();
+//                task = *msit;
+////                        BPS_LOG(INFO) << "task:" << task->tensor_name << " ,size" << task->len << " ,dynamic:" << dynamic_size;
+//                if (task -> len < dynamic_size || (duration_ptr == duration_ptr_len && _dooropen)) {
+//                    if(task -> len < dynamic_size)dynamic_size -= task -> len;
+//                    if(duration_ptr == duration_ptr_len)_dooropen--;
+//                    _ms.erase(_ms.begin());
+//                    BPS_LOG(INFO)  << " ,size" << task->len << "   ,dynamic:" << dynamic_size  <<"   door open value:" << _dooropen << " now pop task:" << task -> priority ;
+//                    task->ready_event = nullptr;
+//                    recorderTs(task);
+//                    return task;
+//                } else {
+////                            BPS_LOG(INFO) << "no space left";
+//                    return nullptr;
+//                }
+//                } else {
+//                    dynamic_size =  durations[++duration_ptr] * B;
+//                    if (duration_ptr < duration_ptr_len)
+//                        next_timer += durations[duration_ptr];
+//                    BPS_LOG(INFO) << "reset:" << next_timer << " dynamic size is: " << dynamic_size << "...............................................";
+//
+//                    return nullptr;
+//                }
+                if (_dooropen > 0) {
+                    msit = _ms.begin();
+                    task = *msit;
+                    _dooropen--;
+                    _ms.erase(_ms.begin());
+                    task->ready_event = nullptr;
+                    recorderTs(task);
+                    return task;
+                } else {
+                    return nullptr;
+                }
+            } else {
+                for (auto it = _sq.begin(); it != _sq.end(); ++it) {
 
-uint32_t BytePSScheduledQueue::pendingSize() {
-  std::lock_guard<std::mutex> lock(_mutex);
-  return _sq.size();
-}
+                    if ((*it)->ready_event) {
+                        if (!(*it)->ready_event->Ready()) {
+                            continue;
+                        }
+                    }
+                    if (_is_scheduled) {
+                        if ((*it)->len > _credits)
+                            continue;
+                    }
+                    if (_rt) {
+                        if (!_rt->IsKeyReady((*it)->key)) {
+                            continue;
+                        }
+                        _rt->ClearReadyCount((*it)->key);
+                    }
+                    task = *it;
+                    if (_is_scheduled) {
+                        _credits -= task->len;
+                    }
+                    _sq.erase(it);
+                    BPS_CHECK(task->tensor_name != "");
+                    BPS_LOG(DEBUG) << "Queue " << LogStrings[_qt]
+                                   << " getTask: " << task->tensor_name << " key: " << task->key
+                                   << " rank: " << BytePSGlobal::GetLocalRank();
+                    task->ready_event = nullptr;
+                    recorderTs(task);
+                    return task;
+                }
+            }
 
-void BytePSScheduledQueue::reportFinish(int size) {
-//  if (_is_scheduled) {
-    std::lock_guard<std::mutex> lock(_mutex);
-    _credits += size;
-//  }
-  return;
-}
+            return nullptr;
+        }
 
-}  // namespace common
+        std::shared_ptr <TensorTableEntry> BytePSScheduledQueue::getTask(uint64_t key) {
+            BPS_CHECK(!_is_scheduled);
+            std::lock_guard <std::mutex> lock(_mutex);
+            std::shared_ptr <TensorTableEntry> task;
+            for (auto it = _sq.begin(); it != _sq.end(); ++it) {
+                if ((*it)->ready_event) {
+                    BPS_CHECK((*it)->ready_event->Ready());
+                }
+                if ((*it)->key != (uint64_t) key) {
+                    continue;
+                }
+                task = *it;
+                _sq.erase(it);
+
+                BPS_CHECK(task->tensor_name != "");
+                BPS_LOG(DEBUG) << "Queue " << LogStrings[_qt]
+                               << " getTask(key): " << task->tensor_name
+                               << " key: " << task->key
+                               << " rank: " << BytePSGlobal::GetLocalRank();
+                task->ready_event = nullptr;
+                recorderTs(task);
+                return task;
+            }
+            return nullptr;
+        }
+
+        uint32_t BytePSScheduledQueue::pendingSize() {
+            std::lock_guard <std::mutex> lock(_mutex);
+            return _sq.size();
+        }
+
+        void BytePSScheduledQueue::reportFinish(std::shared_ptr < TensorTableEntry > task) {//
+            std::lock_guard <std::mutex> lock(_mutex);
+            if (_is_scheduled) {
+                _credits += task -> len;
+            }
+            if (_qt == PUSH && (task -> tensor_name).find("gradient") != (task -> tensor_name).npos) {
+                if  (_dooropen < 11)
+                    _dooropen++;
+            }
+            return;
+        }
+
+    }  // namespace common
 }  // namespace byteps
--- byteps/common/scheduled_queue.h
+++ byteps/common/scheduled_queue.h
@@ -20,34 +20,96 @@
 #include <memory>
 #include <unordered_map>
 #include <vector>
+#include <stack>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
 #include "common.h"
 #include "ready_table.h"
+#include <sys/timeb.h>
 
 namespace byteps {
-namespace common {
+    namespace common {
+        class BytePSScheduledQueue {
+        public:
+            BytePSScheduledQueue(QueueType type);
 
-class BytePSScheduledQueue {
- public:
-  BytePSScheduledQueue(QueueType type);
-  QueueType getQueueType() { return _qt; }
-  void addTask(std::shared_ptr<TensorTableEntry>);
-  void recorderTs(std::shared_ptr<TensorTableEntry>);
-  std::shared_ptr<TensorTableEntry> getTask();
-  std::shared_ptr<TensorTableEntry> getTask(uint64_t key);
-  uint32_t pendingSize();
-  void reportFinish(int size);
+            QueueType getQueueType() { return _qt; }
 
- private:
-  // TODO: use priority queue or heap
-  std::vector<std::shared_ptr<TensorTableEntry>> _sq;
-  std::mutex _mutex;
-  uint64_t _credits;
-  bool _is_scheduled;
-  QueueType _qt;
-  ReadyTable *_rt;
-};
+            void addTask(std::shared_ptr <TensorTableEntry>);
 
-}  // namespace common
+            void recorderTs(std::shared_ptr <TensorTableEntry>);
+
+            std::shared_ptr <TensorTableEntry> getTask();
+
+            std::shared_ptr <TensorTableEntry> getTask(uint64_t key);
+
+            std::multiset < std::shared_ptr < TensorTableEntry >> ::iterator findTask(int priority);
+
+            uint32_t pendingSize();
+
+            void reportFinish(std::shared_ptr <TensorTableEntry> task);
+
+        private:
+            struct comparator {
+                bool operator()(std::shared_ptr <TensorTableEntry> a, std::shared_ptr <TensorTableEntry> b) {
+                    return (a->priority > b->priority);
+                }
+            };
+            std::vector <std::shared_ptr<TensorTableEntry>> _sq;
+            std::multiset <std::shared_ptr<TensorTableEntry>, comparator> _ms;
+            std::vector <std::shared_ptr<TensorTableEntry>> _mysq;
+            std::multiset <int, std::greater<int> > _mystack;
+            std::stack<int> _mystackpull;
+            std::mutex _mutex;
+            uint64_t _credits;
+            bool _is_scheduled;
+            int _tensor_part[160] = {0};
+            int _meetzero = 0;
+            int _dooropen = 11;
+            int _pulldoor = 0;
+            int batchsize = 64;
+            int _grad_checkpoint[13] = {-1, 9, 22, 35, 50, 62, 77, 90, 103, 117, 130, 143, 156};
+            int B = 125000 ;
+            double _backward_exec[13] = {47, 46, 26, 30, 37, 53, 44, 64, 90, 74, 58, 15, 0};
+//            int _forward_exec[13] = {0, 1350000, 1400000, 840000, 900000, 1275000, 1620000, 1335000, 1900000, 2700000,
+//                                     2200000, 1750000, 0};
+            int _exec_stage = 0;
+            int pull_num = 0;
+            int pulled_num = 0;
+            int _noleftsize = 0;
+            int forward_dynamic_size;
+            int _sizepointer = 0;
+            int _stagepullnum = 0;
+            int _dequeue = 0;
+            int _stagestart = 1;
+            int dynamic_size = 0;
+            int _pushsize = 0;
+            int _pullsize = 0;
+            int expected_priority;
+            QueueType _qt;
+            ReadyTable *_rt;
+            //added by qi
+            int _init_pointer = 12;
+            int _pointer;
+            std::string begin_name = "byteps.gradient_144";
+            long long timer = 0;
+//            int duration = 50;
+            int durations[6] = {50, 13, 16, 6, 5, 0};
+            int duration_ptr = 0;
+            int duration_ptr_len = 5;
+            long long next_timer = 0;
+            long long getSystemTime(){
+                timeb t;
+                ftime(&t);
+                return t.time * 1000 + t.millitm;
+            }
+            int max_dynamic_size = 1000000;
+
+
+
+        };
+    }  // namespace common
 }  // namespace byteps
 
 #endif  // BYTEPS_SCHEDULED_QUEUE_H
