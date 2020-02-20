e0d1e26c57c011a1103bc7e2375ea9c0a23f3fd7
jxtxzzw <jxtxzzw@126.com>
2020-02-20 14:28:57 +0800

    fix bug
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- byteps/common/scheduled_queue.cc
+++ byteps/common/scheduled_queue.cc
@@ -161,8 +161,8 @@ namespace byteps {
             std::multiset < std::shared_ptr < TensorTableEntry >> ::iterator msit;
             if (_qt == PUSH) {
                 BPS_LOG(INFO) << "expected_priority=" << expected_priority << ", pointer=" << _pointer << ", dynamic size=" << dynamic_size << ", meetzero=" << _meetzero << ", door=" << _dooropen
+                        << "stack size=" << _mystack.size() << ", ms size=" << _ms.size();
             }
-            << "stack size=" << _mystack.size() << ", ms size=" << _ms.size();
             if (_qt == PUSH && !_dequeue && _ms.size() > 0) {
                 while (_tensor_part[expected_priority] > 0) {
                     for (int x = 0; x < _tensor_part[expected_priority]; x++) {
