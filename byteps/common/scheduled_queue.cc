3ace946ba7ffdcdc6beaf0204d531f59d5affd8a
jxtxzzw <jxtxzzw@126.com>
2020-02-20 14:21:25 +0800

    output log only in PUSH
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- byteps/common/scheduled_queue.cc
+++ byteps/common/scheduled_queue.cc
@@ -159,7 +159,9 @@ namespace byteps {
             std::lock_guard <std::mutex> lock(_mutex);
             std::shared_ptr <TensorTableEntry> task;
             std::multiset < std::shared_ptr < TensorTableEntry >> ::iterator msit;
-            BPS_LOG(INFO) << "expected_priority=" << expected_priority << ", pointer=" << _pointer << ", dynamic size=" << dynamic_size << ", meetzero=" << _meetzero << ", door=" << _dooropen
+            if (_qt == PUSH) {
+                BPS_LOG(INFO) << "expected_priority=" << expected_priority << ", pointer=" << _pointer << ", dynamic size=" << dynamic_size << ", meetzero=" << _meetzero << ", door=" << _dooropen
+            }
             << "stack size=" << _mystack.size() << ", ms size=" << _ms.size();
             if (_qt == PUSH && !_dequeue && _ms.size() > 0) {
                 while (_tensor_part[expected_priority] > 0) {
