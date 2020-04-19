b1ef59f8e1aa2693fb3d0a3cb2d40b19a2c144ae
jxtxzzw <jxtxzzw@126.com>
2020-04-19 19:33:55 +0800

    bin search to find the bug
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- byteps/common/global.cc
+++ byteps/common/global.cc
@@ -41,7 +41,7 @@ uint32_t BytePSGlobal::_partition_bytes = 4096000;
 
 //added by chris
 int BytePSGlobal::pushsize[20] = {0};
-
+int BytePSGlobal::pre_run = true;
 int BytePSGlobal::_is_trace = 0;
 int BytePSGlobal::_start_step = 10;
 int BytePSGlobal::_end_step = 20;
