edcdd9b50c96bbdb74b89340dff0a9bd0e1b7997
jxtxzzw <jxtxzzw@126.com>
2020-04-23 22:24:48 +0800

    add more info
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- byteps/common/scheduled_queue.cc
+++ byteps/common/scheduled_queue.cc
@@ -111,7 +111,6 @@ void BytePSScheduledQueue::addTask(std::shared_ptr<TensorTableEntry> entry) {
   if (BytePSGlobal::pre_run) {
     _sq.push_back(entry);
     if (_qt == PUSH && (entry->tensor_name).find(tensor_keywords) != (entry->tensor_name).npos) {
-      BPS_LOG(INFO) << (entry->tensor_name);
       int pr = entry->priority * -1;
       if (pr > BytePSGlobal::total_grad) {
         BytePSGlobal::total_grad = pr;
