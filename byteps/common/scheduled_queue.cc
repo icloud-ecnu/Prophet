96c5bb751492cff0bf5aed10cb000e35d64f4d05
jxtxzzw <jxtxzzw@126.com>
2020-05-05 20:21:18 +0800

    v27
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- byteps/common/scheduled_queue.cc
+++ byteps/common/scheduled_queue.cc
@@ -92,7 +92,6 @@ void BytePSScheduledQueue::addTask(std::shared_ptr<TensorTableEntry> entry) {
       pre_run_result_sync = true;
       expected_priority = BytePSGlobal::total_grad - 1;
       _pointer = BytePSGlobal::_grad_checkpoint.size() - 1;
-//      BytePSGlobal::B = 125000;
       BPS_LOG(INFO) << "BytePSGlobal::B = " << BytePSGlobal::B;
       BPS_LOG(INFO) << "expected_priority = " << expected_priority;
       BPS_LOG(INFO) << "_pointer = " << _pointer;
@@ -239,7 +238,7 @@ std::shared_ptr<TensorTableEntry> BytePSScheduledQueue::getTask() {
       if (expected_priority == BytePSGlobal::_grad_checkpoint[_pointer - 1]) {
         _dequeue = 1;
         dynamic_size = (long long)(BytePSGlobal::_backward_exec[_sizepointer++] + 1) * BytePSGlobal::B;
-        BPS_LOG(INFO) << "Ready to dequeue: dynamic_size changed to " << dynamic_size;
+//        BPS_LOG(INFO) << "Ready to dequeue: dynamic_size changed to " << dynamic_size;
       }
       return nullptr;
     } else {
@@ -259,10 +258,12 @@ std::shared_ptr<TensorTableEntry> BytePSScheduledQueue::getTask() {
       task = *msit;
       if (!_meetzero) {
         if (dynamic_size > task->len) {
+          BPS_LOG(INFO) << "Start " << task->priority;
           dynamic_size -= task->len;
           _ms.erase(msit);
           _mystack.pop();
         } else {
+          BPS_LOG(INFO) << "No size, enqueue";
           _dequeue = 0;
           if (_pointer > 0) {
             _pointer--;
@@ -274,6 +275,7 @@ std::shared_ptr<TensorTableEntry> BytePSScheduledQueue::getTask() {
       } else if (_bps_credit < task->len) {
         return nullptr;
       } else if (_bps_credit > task->len) {
+        BPS_LOG(INFO) << "After zero, in credit: " << task->priority;
         _bps_credit -= task->len;
         _ms.erase(msit);
         _mystack.pop();
